8aceab24d074b87d3e888b3322ee9ded29628ff4
Tobias <toob0102@gmail.com>
2024-06-19 16:58:32 +0200

    automatic filling of buffer if empty for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
-#include "serial_control.hpp"
 #include "dummy_data_sender.hpp"
+#include "serial_control.hpp"
 
-sen::DummyDataSender dummy_sender{LED_BUILTIN, 30};
-sen::SerialControl serial_control{dummy_sender, 1};
+sen::DummyDataSender dummy_sender{ LED_BUILTIN, 30 };
+sen::SerialControl serial_control{ dummy_sender, 1 };
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(9600);
-    // while(!Serial){
-    //     digitalWrite(LED_BUILTIN, HIGH);
-    // }
-    // digitalWrite(LED_BUILTIN, LOW);
-    // vTaskDelay(2000);
+    pinMode( LED_BUILTIN, OUTPUT );
+    Serial.begin( 9600 );
     serial_control.activate();
 }
 
 void loop() {
+    if ( serial_control.getMeasurementCount() <= 0 ) {
+        Serial.println("==DEBUG== Adding measurements to buffer");
+        serial_control.addMeasure( 10 );
+        serial_control.addMeasure( 20 );
+        serial_control.addMeasure( 30 );
+        serial_control.addMeasure( 40 );
+    }
     taskYIELD();
 }
