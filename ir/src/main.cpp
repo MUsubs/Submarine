6c0b43371147616003411ca2cfde4de2d0c14308
spansos <spaansstan@gmail.com>
2024-06-19 12:40:55 +0200

    actually decent main

--- ir/src/main.cpp
+++ ir/src/main.cpp
@@ -2,32 +2,64 @@
 #include "message_decoder.hpp"
 #include "send_ir_control.hpp"
 
+#include <unordered_set>
 #include <FreeRTOS.h>
 #include <semphr.h>
 #include <task.h>
 #include <Arduino.h>
 
-// #include <unordered_set>
-// std::unordered_set<int> values;
+#define UNIT_LENTGHT 1500
+
+template<typename T>
+class SharedSet {
+public:
+    SharedSet( ) : _mutex( xSemaphoreCreateMutex( ) ) { }
+    void add( T value ) {
+        xSemaphoreTake( _mutex, portMAX_DELAY );
+        _set.insert( value );
+        xSemaphoreGive( _mutex );
+    }
+    bool contains( T value ) {
+        xSemaphoreTake( _mutex, portMAX_DELAY );
+        bool r = _set.find( value ) == _set.end();
+        xSemaphoreGive( _mutex );
+        return r;
+    }
+    void remove( T value ) {
+        xSemaphoreTake( _mutex, portMAX_DELAY );
+        _set.erase( value );
+        xSemaphoreGive( _mutex );
+    }
+    std::unordered_set<T> copy_set( ) {
+        return _set;
+    }
+private:
+    std::unordered_set<T> _set;
+    SemaphoreHandle_t _mutex;
+};
+
+SharedSet<uint32_t> start_values;
+SharedSet<uint32_t> send_values;
+SharedSet<uint32_t> received_values;
 
 class PrintListener : public sen::MessageListener {
 public:
     void messageReceived( uint32_t msg ) override {
-        // Serial.print( "received:\t" + String(msg, 2) + "\n" );
         Serial.printf( "received: %08x\n", msg );
-        // values.erase( msg );
+        received_values.add( msg );
     }
 };
 
-sen::IrReceiver receiver( 26, 250, 15000 );
-sen::MessageDecoder decoder( receiver, 2500 );
+sen::IrReceiver receiver( 26, UNIT_LENTGHT/17, UNIT_LENTGHT*6 );
+sen::MessageDecoder decoder( receiver, UNIT_LENTGHT );
 PrintListener listener;
-sen::SendIrControl sender;
+sen::SendIrControl sender( 9, UNIT_LENTGHT, 16 );
 
 void setup() {
     // serial monitor init
     Serial.begin( 57600 );
-    // receivier config
+
+    // receivier task
     decoder.setMessageListener( &listener );
     xTaskCreate(
         []( void* ){ receiver.main(); },
@@ -48,16 +80,28 @@ void setup() {
         NULL
     );
 
-    // // set random seed for random()
+    // add some random values to test_values
     randomSeed(2);
-    // for ( int i=0; i<50; i++ )
-    //     values.insert( random() );
+    for ( int i = 0; i < 50; i++ )
+        start_values.add( random() );
+    for ( auto i : start_values.copy_set() )
+        send_values.add( i );
 }
 
-void loop() { 
-    int msg = random();
-    Serial.printf( "sending: %08x\n", msg );
-    sender.sendMessage( msg );
-    // Serial.printf("Thingies left:\t%i\n", values.size());
-    delay(2123);
+void loop() {
+    auto set = send_values.copy_set();
+    auto msg_i = set.begin();
+    send_values.remove( *msg_i );
+
+    Serial.printf( "sending: %08x\n", *msg_i );
+    sender.sendMessage( *msg_i );
+
+    int correct_msg_count = 0;
+    for ( auto i : start_values.copy_set() )
+        if ( received_values.contains( i ) )
+            correct_msg_count++;
+
+    Serial.printf( "Thingies received:\t%i\n", correct_msg_count );
+
+    delay( 2000 );
 }
