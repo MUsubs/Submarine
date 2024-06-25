b6aa139473fc07bef27dc856c1aee4fa8a2a48f7
Felix <felix.klaassen@student.hu.nl>
2024-06-25 11:32:44 +0200

    multithreading for aansturingssysteem. Ready for merge.

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,92 @@
 #include <Arduino.h>
+#include <FreeRTOS.h>
 
+#include "dummyControl.hpp"
 #include "motor.hpp"
 #include "motor_control.hpp"
 #include "mpu6050.hpp"
+#include "steer_control.hpp"
+#include "travel_control.hpp"
 
-
-
-// aansturing 1 main:
-
-using namespace asn;
+xTaskHandle motor_control_task_handle;
+xTaskHandle steer_control_task_handle;
+xTaskHandle travel_control_task_handle;
+xTaskHandle dummy_control_task_handle;
 
 static uint8_t motor_pins[7] = { 22, 21, 20, 19, 18, 12, 13 };
 static uint8_t button_pins[4] = { 16, 17, 26, 27 };
-MotorControl motorControl( motor_pins );
-MPU6050 mpu( Wire );
 Kalman kalmanFilter;
-Mpu6050 gyro( mpu, kalmanFilter );
+MPU6050 gyro( Wire );
+asn::Mpu6050 mpu( gyro );
+asn::MotorControl motor_control( motor_pins );
+asn::SteerControl steer_control( mpu, motor_control, kalmanFilter );
+asn::TravelControl travel_control( motor_control, steer_control );
+asn::DummyControl dummy_control( travel_control );
+
+void motorControlTask( void* pvParameters ) {
+    asn::MotorControl* mc = reinterpret_cast<asn::MotorControl*>( pvParameters );
+    mc->main();
+}
+
+void steerControlTask( void* pvParameters ) {
+    asn::SteerControl* sc = reinterpret_cast<asn::SteerControl*>( pvParameters );
+    sc->main();
+}
+
+void travelControlTask( void* pvParameters ) {
+    asn::TravelControl* tc = reinterpret_cast<asn::TravelControl*>( pvParameters );
+    tc->main();
+}
+
+void dummyControlTask( void* pvParameters ) {
+    asn::DummyControl* dc = reinterpret_cast<asn::DummyControl*>( pvParameters );
+    dc->main();
+    vTaskDelete( dummy_control_task_handle );
+}
 
 void setup() {
-    Wire.begin();
-    gyro.setUpGyro();
     Serial.begin( 115200 );
-    adc_init();
-    adc_set_temp_sensor_enabled( true );
-    adc_select_input( 4 );
+    Wire.begin();
+    steer_control.setUpSteerControl();
+
+    vTaskDelay( 5000 );
+
+    Serial.println( "Creating tasks..." );
+    auto return_motor = xTaskCreate( motorControlTask, "MotorControl task", 2048, (void*)&motor_control, 1, &motor_control_task_handle );
+    auto return_steer = xTaskCreate( steerControlTask, "SteerControl task", 2048, (void*)&steer_control, 1, &steer_control_task_handle );
+    auto return_travel = xTaskCreate(travelControlTask, "TravelControl task", 2048, (void*)&travel_control, 1, &travel_control_task_handle);
+    auto return_dummy = xTaskCreate( dummyControlTask, "DummyControl task", 2048, (void*)&dummy_control, 2, &dummy_control_task_handle );
 }
 
 void loop() {
-    Serial.print( "graden: " + String( round( gyro.getCurrent_z() ) ) );
-    if ( round( gyro.getCurrent_z() ) < gyro.getSetpoint() ) {
-        Serial.println( " left " );
-        motorControl.move( motorControl.direction_t::LEFT );
-        delay( 50 );
-    } else if ( round( gyro.getCurrent_z() ) > gyro.getSetpoint() ) {
-        Serial.println( " right " );
-        motorControl.move( motorControl.direction_t::RIGHT );
-        delay( 50 );
-    }
-    if ( digitalRead( button_pins[0] ) == HIGH ) {
-        motorControl.move( motorControl.direction_t::FORWARD );
-        delay( 50 );
-        motorControl.move( motorControl.direction_t::STOP );
-    }
-    if ( digitalRead( button_pins[1] ) == HIGH ) {
-        motorControl.move( motorControl.direction_t::BACKWARD );
-        delay( 50 );
-        motorControl.move( motorControl.direction_t::STOP );
-    }
-    if ( digitalRead( button_pins[2] ) == HIGH ) {
-        motorControl.move( motorControl.direction_t::UP );
-        delay( 50 );
-        motorControl.move( motorControl.direction_t::STOP );
-    }
-    if ( digitalRead( button_pins[3] ) == HIGH ) {
-        motorControl.move( motorControl.direction_t::DOWN );
-        delay( 50 );
-        motorControl.move( motorControl.direction_t::STOP );
-    }
+    // Serial.print( "graden: " + String( round( gyro.getCurrent_z() ) ) );
+    // if ( round( gyro.getCurrent_z() ) < gyro.getSetpoint() ) {
+    //     Serial.println( " left " );
+    //     motorControl.move( motorControl.direction_t::LEFT );
+    //     delay( 50 );
+    // } else if ( round( gyro.getCurrent_z() ) > gyro.getSetpoint() ) {
+    //     Serial.println( " right " );
+    //     motorControl.move( motorControl.direction_t::RIGHT );
+    //     delay( 50 );
+    // }
+    // if ( digitalRead( button_pins[0] ) == HIGH ) {
+    //     motorControl.move( motorControl.direction_t::FORWARD );
+    //     delay( 50 );
+    //     motorControl.move( motorControl.direction_t::STOP );
+    // }
+    // if ( digitalRead( button_pins[1] ) == HIGH ) {
+    //     motorControl.move( motorControl.direction_t::BACKWARD );
+    //     delay( 50 );
+    //     motorControl.move( motorControl.direction_t::STOP );
+    // }
+    // if ( digitalRead( button_pins[2] ) == HIGH ) {
+    //     motorControl.move( motorControl.direction_t::UP );
+    //     delay( 50 );
+    //     motorControl.move( motorControl.direction_t::STOP );
+    // }
+    // if ( digitalRead( button_pins[3] ) == HIGH ) {
+    //     motorControl.move( motorControl.direction_t::DOWN );
+    //     delay( 50 );
+    //     motorControl.move( motorControl.direction_t::STOP );
+    // }
 }
-
-
-// aansturing 2 main:
-
-
-// #include "Wire.h"
-// #include "mpu6050.hpp"
-
-
-// VarSpeedServo my_servo;
-// MPU6050 mpu( Wire );
-// Kalman kalmanFilter;
-// Mpu6050 gyro( my_servo, mpu, kalmanFilter);
-// unsigned long timer = 0;
-
-
-// void setup() {
-//     Serial.begin( 9600 );
-//     Wire.begin();
-//     gyro.setUpGyro();
-// }
-
-// void loop() {
-
-//     if ( Serial.available() > 0 ) {
-//       String inputString = Serial.readStringUntil( '\n' );
-//       gyro.setSetpoint( inputString.toFloat() );
-//     }
-
-
-//     gyro.kalman();
-//     my_servo.write( gyro.PID(), 30 );
-//     Serial.println(gyro.mean());
-
-
-//     delay( 50 );
-// }
