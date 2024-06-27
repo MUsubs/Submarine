3ead6cb7ee707c8525f2bdd2cc84954f5b7fc822
Tobias <toob0102@gmail.com>
2024-06-27 15:16:15 +0200

    Test code for @JorgBurg1309

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,9 @@
 #include "message_interpreter.hpp"
 #include "r2d2_debug_macros.hpp"
 #include "message_printer.hpp"
+#include "packet_enums.hpp"
 
-bool is_sub = true;
+bool is_sub = false;
 
 sen::MessageInterpreter message_interpreter{ 20, 1 };
 sen::DataTransceiver data_transceiver{ 10, 9, 2, is_sub, message_interpreter, 1 };
@@ -26,6 +27,23 @@ void setup() {
     data_transceiver.activate();
 }
 
+std::vector<uint8_t> data;
+uint8_t header;
+
 void loop() {
+    // update 0 0 0
+    header = data_transceiver.generateUpdateHeader(sen::data_t::CURR, 3);
+    data = {header, 0, 0, 0};
+    data_transceiver.sendBytes(data);
+
+    // new pos 1 0 1
+    header = data_transceiver.generateInstructionHeader(sen::inst_t::NEW_POS, 3);
+    data = {header, 255, 0, 255};
+    data_transceiver.sendBytes(data);
+
+    // update 0.5 0 0
+    header = data_transceiver.generateUpdateHeader(sen::data_t::CURR, 3);
+    data = {header, 127, 0, 0};
+    data_transceiver.sendBytes(data);
     taskYIELD();
 }
