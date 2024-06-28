dae20d31cb2058342451ef5c397623e93b7697ba
Tobias <toob0102@gmail.com>
2024-06-28 15:52:34 +0200

    Updated main to use MessagePasser, in theory Land-Dev is now ready for testing with new ASN code

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 #include "data_transceiver.hpp"
 #include "message_interpreter.hpp"
-#include "message_printer.hpp"
+#include "message_passer.hpp"
 #include "packet_enums.hpp"
 
 #define R2D2_DEBUG_ENABLE
@@ -15,7 +15,8 @@ bool is_sub = false;
 
 sen::MessageInterpreter message_interpreter{ 20, 1 };
 sen::DataTransceiver data_transceiver{ 10, 9, 2, is_sub, message_interpreter, 1 };
-sen::MessagePrinter message_printer{};
+sen::SerialControl serial_control{ data_transceiver, 1 };
+sen::MessagePasser message_passer{ serial_control, 1 };
 
 void setup() {
     Serial.begin( 115200 );
@@ -23,52 +24,17 @@ void setup() {
     while ( !Serial );
     vTaskDelay( 2000 );
 
-    message_interpreter.setListener( &message_printer );
+    message_interpreter.setListener( &message_passer );
 
     message_interpreter.activate();
     data_transceiver.activate();
+    serial_control.activate();
+    message_passer.activate();
 }
 
 std::vector<uint8_t> data;
 uint8_t header;
 
 void loop() {
-    // // update 0 0 0
-    // header = data_transceiver.generateUpdateHeader(sen::data_t::CURR, 3);
-    // data = {header, 0, 0, 0};
-    // data_transceiver.sendBytes(data);
-    // Serial.println("Data:");
-    // for (auto b : data){
-    //     Serial.printf("%d\n", b);
-    // }
-    // Serial.println();
-    // vTaskDelay(500);
-
-    // new pos 1 0 1
-    int time = 200;
-
-    header = data_transceiver.generateInstructionHeader( sen::inst_t::NEW_POS, 3 );
-    data = { header, 255, 0, 255 };
-    data_transceiver.sendBytes( data );
-    R2D2_DEBUG_LOG( "sending NEW_POS 255, 0, 255" );
-    vTaskDelay( time );
-
-    header = data_transceiver.generateInstructionHeader( sen::inst_t::ACK, 0 );
-    data = { header };
-    data_transceiver.sendBytes( data );
-    R2D2_DEBUG_LOG( "sending ACK" );
-    vTaskDelay( time );
-
-    header = data_transceiver.generateUpdateHeader( sen::data_t::CURR, 3 );
-    data = { header, 127, 255, 127 };
-    data_transceiver.sendBytes( data );
-    R2D2_DEBUG_LOG( "sending UPDATE 127, 255, 127" );
-    vTaskDelay( time );
-
-    header = data_transceiver.generateSensorHeader( sen::sens_t::TEMP, 2 );
-    data = { header, 30, 50 };
-    data_transceiver.sendBytes( data );
-    R2D2_DEBUG_LOG( "sending SENS 30.5" );
-    vTaskDelay( time );
     taskYIELD();
 }
